8330c8ac18bace183ff8c9e666fa7f2c59fcd7c6
Gessoo <115876483+Gessoo@users.noreply.github.com>
2026-07-20 17:08:52 +0800

    增加了RSS，完成了所有教学，一知半解。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://astrotry1.netlify.app/"
+  site: "https://astrotry1.netlify.app/",
+  integrations: [preact()]
 });
